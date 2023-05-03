090edfab81ac1bd59e2efb26491e2d57e12cbc55
Jamesgit22 <jschroeder97@icloud.com>
2023-05-02 20:26:45 -0500

    Added all endpoints for category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,64 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+  try {
+    const allCats = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(allCats);
+  } catch (err) {
+    throw err;
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    const oneCat = await Category.findOne({
+      where: { id: req.params.id },
+      include: [{ model: Product }],
+    });
+    res.status(200).json(oneCat);
+  } catch (err) {
+    res.status(500).json("Not sure what broke");
+    throw err;
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCat = Category.create(req.body);
+    res.status(200).json("Success");
+  } catch (err) {
+    throw err;
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCat = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json("Success");
+  } catch (err) {
+    throw err;
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const deleteCat = await Category.destroy({ where: {id: req.params.id}})
+    res.status(200).json('Successfully Deleted');
+  } catch (err) {
+    throw err;
+  }
 });
 
 module.exports = router;
